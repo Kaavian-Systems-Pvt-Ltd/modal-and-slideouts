285583e6c8d63ffdf418a1e656c3fb43009526dd
mhdSamsu <mohammedsamsuthin.k@kaaviansys.com>
2022-12-04 17:16:47 +0530

    modal init

---
+++ src/components/modals/ModalComponets.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CheckCircleOutline , ErrorOutline , WarningAmberRounded } from '@mui/icons-material'; 
+import securityImg from '../img/Vectorsecurity.png';
+
+import './style.css';
+
+export const ModalHeader = ({ title , mode })=> 
+{
+    const SecurityAlert = ()=> 
+    {
+        return (
+            <div className='alert--security'>
+                <img src={securityImg} alt='security' />
+                <h2>{title}</h2>
+            </div>
+        );
+    };
+    return (
+        <div className='modalHeader'>
+            {mode === 'alert--success' ? <CheckCircleOutline  className='alert--success' sx={{ 'fontSize' : 50 }}/> : null }
+            {mode === 'alert--error' ? <ErrorOutline  className='alert--error' sx={{ 'fontSize' : 50 }}/> : null }
+            {mode === 'alert--warn' ? <WarningAmberRounded  className='alert--warn' sx={{ 'fontSize' : 50 }} /> : null }
+            {mode === 'alert--security' ? <SecurityAlert /> : null }
+            {mode === 'info' || mode === 'normal' ? <h2>{title}</h2> : null }
+            
+        </div>
+    );
+};
+
+ModalHeader.propTypes = {
+    'title' : PropTypes.string.isRequired ,
+    'mode' : PropTypes.string.isRequired
+};
+
+export const ModalButton =  ({ mode , onPositive , onNegative })=> 
+{
+    return(
+        <div className='modalButton'>
+            <button className='btn positive' onClick={onPositive?.onClick}>{onPositive?.label}</button>
+            <button className='btn negative' onClick={onNegative?.onClick}>{onNegative?.label}</button> 
+        </div>
+    );
+};
+
+ModalButton.propTypes = {
+    'mode' : PropTypes.string.isRequired ,
+    'onPositive' : PropTypes.shape ({ 
+        'label' : PropTypes.string.isRequired , 
+        'onClick' : PropTypes.func.isRequired 
+    }).isRequired ,
+    'onNegative' : PropTypes.shape ({ 
+        'label' : PropTypes.string.isRequired , 
+        'onClick' : PropTypes.func.isRequired 
+    })
+};
--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -1,194 +1,310 @@
-import React,{useState} from "react";
+import React , { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-
+import PropTypes from 'prop-types';
 import { Modal } from '../components/modals';
 
-const stories = storiesOf('Modal', module);
+const stories = storiesOf ('Modal' , module);
 
-const ModalSkeleton = ({modalProbs,clickHandler,msg}) =>{
-    const Content = () =>{
+const ModalSkeleton = ({ modalProbs , clickHandler , msg })=> 
+{
+    const Content = ()=> 
+    {
         return(
             <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, possimus sapiente facilis 
                 illum temporibus laudantium. Quaerat recusandae delectus suscipit, 
                 asperiores molestias voluptatum corrupti modi velit. Maxime, assumenda! Nisi, officia dolore! 
             </p>
         );
-    }
+    };
 
     return(
-            <div className="container">
+            <div className='container'>
                     <button onClick={clickHandler}> clik to view modal</button>
                     <h2>{msg}</h2>
                     <Modal Body={Content}{...modalProbs}/>
             </div>
     );
-}
+};
+
+ModalSkeleton.propTypes = {
+    'modalProbs' : PropTypes.object.isRequired , 
+    'clickHandler' : PropTypes.func.isRequired , 
+    'msg' : PropTypes.string.isRequired
+};
 
 // Use Cases
 
-stories.add('Simple Msg', () =>{
-    const [status, setStatus] = useState('hidden');
-    const [msg, setMsg] = useState('');
+stories.add ('Info Modal' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
 
-    const clickHandler = () =>{
-        setMsg('')
-        setStatus('visible') 
-    }
+    const clickHandler = ()=> 
+    {
+        setMsg ('');
+        setStatus ('visible'); 
+    };
 
-    const onPositive = () =>{
-        setMsg('Operation Success');
-        setStatus('hidden');
-    }
+    const onPositive = ()=> 
+    {
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
 
     const modalProbs = {
-        visibility:status,
-        mode:'simple',
-        size:'small',
-        header:'Simple Msg',
-        onPositive:{
-            label    : 'Okey',
-            onClick : onPositive
+        'visibility' : status ,
+        'mode' : 'info' ,
+        'size' : 'small' ,
+        'title' : 'Simple Msg' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
         }
-    }
+    };
     return(
-            <div className="container">
+            <div className='container'>
                 <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
             </div>
     );
-})
+});
 
-stories.add('Alert Msg', () =>{
-    const [status, setStatus] = useState('hidden');
-    const [msg, setMsg] = useState('');
+stories.add ('Alert Success' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
 
-    const clickHandler = () =>{
-        setMsg('')
-        setStatus('visible') 
-    }
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
 
-    const onPositive = () =>{
-        setMsg('Operation Success');
-        setStatus('hidden');
-    }
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
 
     const modalProbs = {
-        visibility:status,
-        mode:'alert',
-        size:'medium',
-        header:'Alert Msg',
-        onPositive:{
-            label    : 'Okey',
-            onClick : onPositive
-        },
-        onNegative :{
-            label    : 'Cancle',
-            onClick : () => setStatus('hidden')
+        'visibility' : status ,
+        'mode' : 'alert--success' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
         } 
-    }
+    };
     return(
-            <div className="container">
+            <div className='container'>
                 <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
             </div>
     );
-})
+});
 
-stories.add('small', () =>{
-    const [status, setStatus] = useState('hidden');
-    const [msg, setMsg] = useState('');
+stories.add ('Alert Error' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
 
-    const clickHandler = () =>{
-        setMsg('')
-        setStatus('visible') 
-    }
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
 
-    const onPositive = () =>{
-        setMsg('Operation Success');
-        setStatus('hidden');
-    }
-
-    const modalProbs = {
-        visibility:status,
-        mode:'alert',
-        size:'small',
-        header:'Small',
-        onPositive:{
-            label    : 'Okey',
-            onClick : onPositive
-        },
-        onNegative :{
-            label    : 'Cancle',
-            onClick : () => setStatus('hidden')
-        } 
-    }
-    return(
-            <div className="container">
-                <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
-            </div>
-    );
-})
-
-stories.add('medium', () =>{
-    const [status, setStatus] = useState('hidden');
-    const [msg, setMsg] = useState('');
-
-    const clickHandler = () =>{
-        setMsg('')
-        setStatus('visible') 
-    }
-
-    const onPositive = () =>{
-        setMsg('Operation Success');
-        setStatus('hidden');
-    }
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
 
     const modalProbs = {
-        visibility:status,
-        mode:'simple',
-        size:'medium',
-        header:'Medium',
-        onPositive:{
-            label    : 'Okey',
-            onClick : onPositive
+        'visibility' : status ,
+        'mode' : 'alert--error' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
         }
-    }
+    };
     return(
-            <div className="container">
+            <div className='container'>
                 <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
             </div>
     );
-})
+});
+stories.add ('Alert Warning' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
 
-stories.add('Large', () =>{
-    const [status, setStatus] = useState('hidden');
-    const [msg, setMsg] = useState('');
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
 
-    const clickHandler = () =>{
-        setMsg('')
-        setStatus('visible') 
-    }
-
-    const onPositive = () =>{
-        setMsg('Operation Success');
-        setStatus('hidden');
-    }
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
 
     const modalProbs = {
-        visibility:status,
-        mode:'alert',
-        size:'large',
-        header:'Large',
-        onPositive:{
-            label    : 'Okey',
-            onClick : onPositive
-        },
-        onNegative :{
-            label    : 'Cancel',
-            onClick : () => setStatus('hidden')
-        } 
-    }
+        'visibility' : status ,
+        'mode' : 'alert--warn' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
+        }
+    };
     return(
-            <div className="container">
+            <div className='container'>
                 <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
             </div>
     );
-})
+});
+stories.add ('Alert Security' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
+
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
+
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
+
+    const modalProbs = {
+        'visibility' : status ,
+        'mode' : 'alert--security' ,
+        'title' : 'Alert Security' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
+        } ,
+        'onNegative' : {
+            'label' : 'Cancle' ,
+            'onClick' : ()=> setStatus ('hidden')
+        } 
+    };
+    return(
+            <div className='container'>
+                <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
+            </div>
+    );
+});
+
+stories.add ('Normal small' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
+
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
+
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
+
+    const modalProbs = {
+        'visibility' : status ,
+        'mode' : 'normal' ,
+        'size' : 'small' ,
+        'title' : 'Normal Small' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
+        } ,
+        'onNegative' : {
+            'label' : 'Cancle' ,
+            'onClick' : ()=> setStatus ('hidden')
+        } 
+    };
+    return(
+            <div className='container'>
+                <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
+            </div>
+    );
+});
+
+stories.add ('medium' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
+
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
+
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
+
+    const modalProbs = {
+        'visibility' : status ,
+        'mode' : 'simple' ,
+        'size' : 'medium' ,
+        'title' : 'Medium' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
+        }
+    };
+    return(
+            <div className='container'>
+                <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
+            </div>
+    );
+});
+
+stories.add ('Large' , ()=> 
+{
+    const [ status , setStatus ] = useState ('hidden');
+    const [ msg , setMsg ] = useState ('');
+
+    const clickHandler = ()=> 
+{
+        setMsg ('');
+        setStatus ('visible'); 
+    };
+
+    const onPositive = ()=> 
+{
+        setMsg ('Operation Success');
+        setStatus ('hidden');
+    };
+
+    const modalProbs = {
+        'visibility' : status ,
+        'mode' : 'alert' ,
+        'size' : 'large' ,
+        'title' : 'Large' ,
+        'onPositive' : {
+            'label' : 'Okey' ,
+            'onClick' : onPositive
+        } ,
+        'onNegative' : {
+            'label' : 'Cancel' ,
+            'onClick' : ()=> setStatus ('hidden')
+        } 
+    };
+    return(
+            <div className='container'>
+                <ModalSkeleton modalProbs ={modalProbs} status={status} clickHandler={clickHandler} msg={msg} />
+            </div>
+    );
+});
